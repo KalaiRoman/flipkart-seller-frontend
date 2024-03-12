19102223571eb6cd7e67c13699b0e2c3451c3f70
kalai <kalai@cdp360.com>
2024-03-13 00:16:02 +0530

    folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      seller app amma appa page welcome
+    </div>
+  );
+}
+
+export default App;
